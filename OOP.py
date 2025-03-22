b10e1cd2651413cc16283084288cc2be326ff2ca
AneliBem <tolya.bem@gmail.com>
2025-03-22 19:02:03 +0200

    Encapsulation

--- OOP.py
+++ OOP.py
@@ -3,7 +3,22 @@ class Dog:
        self.name = name
        self.age = age
 
+    def __sit(self):
+        return my_dog.name + " is now sitting."
+
+    def _sit(self):
+        return my_dog.name + " is now sitting."
+    
+    def sit(self):
+        return my_dog.name + " is now sitting."
+
 
 my_dog = Dog("John", 10)    
 
 print(my_dog.name)
+
+# Encapsulation
+
+print(my_dog._Dog__sit())
+print(my_dog._sit())
+print(my_dog.sit())
