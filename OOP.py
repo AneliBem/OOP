4a902ae6f977a193dc7752d1beca911f125a2d9b
AneliBem <tolya.bem@gmail.com>
2025-03-23 19:15:44 +0200

    + methods

--- OOP.py
+++ OOP.py
@@ -4,20 +4,20 @@ class Dog:
        self.age = age
 
     def __sit(self):
-        return my_dog.name + " is now sitting."
+        return my_dog.name.title() + " is now sitting."
 
-    def _sit(self):
-        return my_dog.name + " is now sitting."
+    def _roll(self):
+        return my_dog.name.title() + " is now roll."
     
     def sit(self):
-        return my_dog.name + " is now sitting."
+        return my_dog.name.capitalize() + " is now stand up."
 
 
-my_dog = Dog("John", 10)    
+my_dog = Dog("john", 10)    
 
 print(my_dog.name)
 
 # Encapsulation
 print(my_dog._Dog__sit())
-print(my_dog._sit())
+print(my_dog._roll())
 print(my_dog.sit())
