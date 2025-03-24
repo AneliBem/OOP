52c0b765f27c4cb7a55691754ec476e35e81c18b
AneliBem <tolya.bem@gmail.com>
2025-03-24 16:50:07 +0200

    Inheritance

--- OOP.py
+++ OOP.py
@@ -1,8 +1,19 @@
-class Dog:
+class Animal:
     def __init__(self, name, age):
-       self.name = name
-       self.age = age
+        self.name = name
+        self.age = age
 
+    def speak(self):
+        return "Some speak"
+class Cat(Animal):
+    def speak(self):
+        return "Neow"
+
+class Dog(Animal):
+
+    def speak(self):
+        return "Wof"
+    
     def __sit(self):
         return my_dog.name.title() + " is now sitting."
 
@@ -13,11 +24,15 @@ class Dog:
         return my_dog.name.capitalize() + " is now stand up."
 
 
-my_dog = Dog("john", 10)    
+my_dog = Dog("john", 10)
+my_cat = Cat("Bars", 5)    
 
-print(my_dog.name)
+# print(my_dog.name)
 
 # Encapsulation
-print(my_dog._Dog__sit())
-print(my_dog._roll())
-print(my_dog.sit())
+# print(my_dog._Dog__sit())
+# print(my_dog._roll())
+# print(my_dog.sit())
+
+print(my_cat.name, " speak to ", my_cat.speak())
+print(my_dog.name.capitalize(), " speak to ", my_dog.speak())
